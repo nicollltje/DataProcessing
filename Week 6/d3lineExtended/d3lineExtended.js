f585857bfd9c3da9bad311ac2d1a21d4d0203a25
Nicol Heijtbrink <Nicol@Nicols-MacBook-Pro.local>
2016-12-10 00:02:15 +0100

    part 3: being able to choose between Maastricht and Leeuwarden

--- Week 6/d3lineExtended/d3lineExtended.js
+++ Week 6/d3lineExtended/d3lineExtended.js
@@ -1,5 +1,9 @@
 
 d3.json("weatherdata1994.json", function(data){
+	// Set up stack method
+	var stack = d3.layout.stack();
+	dataset = data;
+
 	// size of the graph
 	var margin = {top: 20, left:  50, right: 20, bottom: 80},
 		width = 900 - margin.left - margin.right,
@@ -37,12 +41,8 @@ d3.json("weatherdata1994.json", function(data){
 			//console.log(d.date, d.temperatures)
 			return d;
 	});
-	// for (i = 0; i < data.length; i++){
-	// 	d.date = d.Leeuw[i].temperatures[2]
-	// 	console.log(d.date)
-	// }
-	console.log(d3.extent(data, function(d) { if(d.city =="Leeuwarden"){
-		return d.maxTemperature; }}))
+
+	// set the domains for the graph
 	x.domain(d3.extent(data, function(d) { if(d.city =="Leeuwarden"){
 		return d.date; }}));
 	y.domain([
@@ -50,25 +50,101 @@ d3.json("weatherdata1994.json", function(data){
 		d3.max(data, function(d) { if(d.city =="Leeuwarden"){
 		return d.maxTemperature; }})
 		]);
-	// make a line from the datapoints
+	
+	// make a line from the datapoints for minimum temperature
 	var lineMin = d3.svg.line()
 		.x(function(d) {if(d.city == "Leeuwarden"){
 			return x(d.date); } else{return x.domain} })
 		.y(function(d) {if(d.city == "Leeuwarden"){
 			return y(d.minTemperature); } else{return y.domain} });
 
+	// make a line from the datapoints for maximum temperature
 	var lineMax = d3.svg.line()
 		.x(function(d) {if(d.city == "Leeuwarden"){
 			return x(d.date); } else{return x.domain} })
 		.y(function(d) {if(d.city == "Leeuwarden"){
 			return y(d.maxTemperature); } else{return y.domain} });
 
+	// make a line from the datapoints for average temperature
 	var lineAvg = d3.svg.line()
 		.x(function(d) {if(d.city == "Leeuwarden"){
 			return x(d.date); } else{return x.domain} })
 		.y(function(d) {if(d.city == "Leeuwarden"){
 			return y(d.avgTemperature); } else{return y.domain} });
 
+	d3.selectAll(".m")
+		.on("click", function() {
+			var date = this.getAttribute("value");
+
+			var str;
+			if (date == "Leeuwarden"){
+				console.log("Leeuwarden")
+				str = "Leeuwarden";
+			}
+			if (date == "Maastricht"){
+				console.log("Maastricht")
+				str = "Maastricht";
+			}
+
+			data.forEach(function(d) {
+				d.date = parseDate(d.date);
+				d.city = d.city
+				//console.log(d.city)
+				d.minTemperature = Number(d.temperatures[0])
+				d.maxTemperature = Number(d.temperatures[1])
+				d.avgTemperature = Number(d.temperatures[2])
+				//console.log(d.date, d.temperatures)
+				return d;
+			});
+			// set the domains for the graph
+			x.domain(d3.extent(data, function(d) { if(d.city == str){
+				return d.date; }}));
+			y.domain([
+				d3.min(data, function(d){return d.minTemperature}),
+				d3.max(data, function(d) { if(d.city == str){
+					return d.maxTemperature; }})
+			]);
+
+			// make a line from the datapoints for minimum temperature
+			var lineMin = d3.svg.line()
+				.x(function(d) {if(d.city == str){
+					return x(d.date); } else{return x.domain} })
+				.y(function(d) {if(d.city == str){
+					return y(d.minTemperature); } else{return y.domain} });
+
+			// make a line from the datapoints for maximum temperature
+			var lineMax = d3.svg.line()
+				.x(function(d) {if(d.city == str){
+					return x(d.date); } else{return x.domain} })
+				.y(function(d) {if(d.city == str){
+					return y(d.maxTemperature); } else{return y.domain} });
+
+			// make a line from the datapoints for average temperature
+			var lineAvg = d3.svg.line()
+				.x(function(d) {if(d.city == str){
+			return x(d.date); } else{return x.domain} })
+				.y(function(d) {if(d.city == str){
+					return y(d.avgTemperature); } else{return y.domain} });
+
+			svg.append("path")
+				.datum(data)
+				.attr("class", "lineMin")
+				.style("stroke", "blue")
+				.attr("d", lineMin)
+
+			svg.append("path")
+				.datum(data)
+				.attr("class", "lineMax")
+				.style("stroke", "red")
+				.attr("d", lineMax)
+
+			svg.append("path")
+				.datum(data)
+				.attr("class", "lineAvg")
+				.style("stroke", "yellow")
+				.attr("d", lineAvg)
+		})
+	
 	svg.append("g")
       .attr("class", "x_axis")
       .attr("transform", "translate(0," + height + ")")
@@ -107,29 +183,29 @@ d3.json("weatherdata1994.json", function(data){
       .attr("class", "focus")
       .style("display", "none");
 
-  focus.append("circle")
-      .attr("r", 4.5);
+  	focus.append("circle")
+      	.attr("r", 4.5);
 
-  focus.append("text")
-      .attr("x", 9)
-      .attr("dy", ".35em");
+  	focus.append("text")
+      	.attr("x", 9)
+      	.attr("dy", ".35em");
 
-  svg.append("rect")
-      .attr("class", "overlay")
-      .attr("width", width)
-      .attr("height", height)
-      .on("mouseover", function() { focus.style("display", null); })
-      .on("mouseout", function() { focus.style("display", "none"); })
-      .on("mousemove", mousemove);
+  	svg.append("rect")
+      	.attr("class", "overlay")
+      	.attr("width", width)
+      	.attr("height", height)
+      	.on("mouseover", function() { focus.style("display", null); })
+      	.on("mouseout", function() { focus.style("display", "none"); })
+      	.on("mousemove", mousemove);
 
-  function mousemove() {
-    var x0 = x.invert(d3.mouse(this)[0]),
-        i = bisectDate(data, x0, 1),
-        d0 = data[i - 1],
-        d1 = data[i],
-        d = x0 - d0.date > d1.date - x0 ? d1 : d0;
-    focus.attr("transform", "translate(" + x(d.date) + "," + y(d.avgTemperature) + ")");
-    focus.select("text").text((d.avgTemperature));
+  	function mousemove() {
+    	var x0 = x.invert(d3.mouse(this)[0]),
+        	i = bisectDate(data, x0, 1),
+        	d0 = data[i - 1],
+        	d1 = data[i],
+        	d = x0 - d0.date > d1.date - x0 ? d1 : d0;
+    	focus.attr("transform", "translate(" + x(d.date) + "," + y(d.avgTemperature) + ")");
+    	focus.select("text").text((d.date, d.avgTemperature));
   }
 
 });
